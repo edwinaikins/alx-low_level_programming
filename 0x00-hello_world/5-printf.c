7bf0f9a26286583df13ead8c592d67e2abac89d2
edwinaikins <edwinaikins@gmail.com>
2022-03-10 21:02:03 +0000

    Print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Function to print
@@ -6,6 +5,8 @@
  */
 int main(void)
 {
-write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+write(2, str1, 59);
 return (1);
 }
