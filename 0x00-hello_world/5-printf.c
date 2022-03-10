36a776dab34697baaca09365115b93fbbaff55ed
edwinaikins <edwinaikins@gmail.com>
2022-03-10 20:57:12 +0000

    Print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Function to print
@@ -5,7 +6,6 @@
  */
 int main(void)
 {
-    char str[]="with proper grammar, but the outcome is a piece of art,"
-    write(2,str,59);
+write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 return (1);
 }
