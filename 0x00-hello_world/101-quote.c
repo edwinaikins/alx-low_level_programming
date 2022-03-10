643763ea504e2ff68234f7d3c2caa3607a50f0b0
edwinaikins <edwinaikins@gmail.com>
2022-03-10 21:08:06 +0000

    Print

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 /**
- * main - Print sizeof
- * Return: Always 0 (Success)
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-fwrite(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, str1, 59);
 return (1);
 }
