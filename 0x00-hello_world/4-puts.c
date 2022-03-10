42308fab52e39b8685f4074469abd20ffbbc639c
edwinaikins <edwinaikins@gmail.com>
2022-03-10 13:06:14 +0000

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - Function to print
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle\n");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
