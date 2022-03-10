1f9428147098ea1bef4595a0d981f0adca239f68
edwinaikins <edwinaikins@gmail.com>
2022-03-10 21:10:27 +0000

    Hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
-#include <unistd.h>
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Return: Always 1 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(2, str1, 59);
-	return (1);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
