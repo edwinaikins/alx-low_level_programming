699e79654c2dd8278ee286cf2a0398e9448c2ce1
edwinaikins <edwinaikins@gmail.com>
2022-03-11 16:55:42 -0800

    Mandatory task

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,20 +3,18 @@
 #include <time.h>
 
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-char low;
+	char low;
 
-for (low = 'z'; low >= 'a'; low--)
-{	
-putchar(low);
-}	
-putchar('\n');
+	for (low = 'z'; low >= 'a'; low--)
+		putchar(low);
+	putchar('\n');
 
-return (0);
+	return (0);
 }
