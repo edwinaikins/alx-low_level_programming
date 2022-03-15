4a6068da9cc218f6754047db08b073627c1de20a
edwinaikins <edwinaikins@gmail.com>
2022-03-15 12:23:34 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - Check description
+ * Description: printing
+ * Return: nothing
+ */
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 97; x < 123; x++)
+		_putchar(x);
+	_putchar('\n');
+}
