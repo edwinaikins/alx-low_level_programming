49b4df2c2e049843df827cf8683430ce7c981120
edwinaikins <edwinaikins@gmail.com>
2022-03-17 09:58:23 -0700

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+/**
+ * print_numbers - prints numbers from 0 to 9.
+ * Description
+ * Return: 0 success.
+ */
+void print_numbers(void)
+{
+	int a;
+	
+	for (a = 48; a < 58; a++)
+		_putchar(a);
+	_putchar('\n');
+	return (0);
+}
