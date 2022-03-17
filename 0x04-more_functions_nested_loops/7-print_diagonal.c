5af800a97130944a1dadb3aea7d5d4598664ae2c
edwinaikins <edwinaikins@gmail.com>
2022-03-17 16:33:49 -0700

    Print diagnoal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,11 +6,16 @@
  */
 void print_diagonal(int n)
 {
-	int a;
+	int a, b;
 
 	for (a = 0; a < n; a++)
 	{
+		for (b = 0; b < a; b++)
+		{
+			_putchar(' ');
+		}	
 		_putchar(92);
+		_putchar('\n');
 	}
 	_putchar('\n');
 }
