d40b7dbd26583654145268036a2787b2961ec257
edwinaikins <edwinaikins@gmail.com>
2022-03-17 10:36:45 -0700

    Void print

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints 0 to 9 but ommits 2 and 4
+ * description
+ * Return: nothing
+ */
+void print_most_numbers(void);
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		if (a != 50 || a != 52)
+			_putchar(a);
+	}
+	_putchar('\n');
+}
