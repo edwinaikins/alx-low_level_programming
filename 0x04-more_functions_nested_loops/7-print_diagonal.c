6118404385f1f5b4cdd17c1dce9c6ff5f93860b1
edwinaikins <edwinaikins@gmail.com>
2022-03-17 16:19:06 -0700

    Print diagnoal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_diagonal - function to print diagonal
+ * @n: input
+ * Return: nothing
+ */
+void print_diagonal(int n)
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+	{
+		_putchar(92);
+	}
+	_putchar('\n');
+}
