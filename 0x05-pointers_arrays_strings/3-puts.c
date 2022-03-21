c37d4c0777bb3b06450d0a95a7bffd209f7aa3f4
edwinaikins <edwinaikins@gmail.com>
2022-03-21 16:02:29 -0700

    Print

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - function to print some strings
+ * @str: input
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	int count = 0;
+
+	while (str[count] != '\0')
+	{	
+		_putchar(str[count]);
+		count++;
+	}
+	_putchar('\n');
+}
