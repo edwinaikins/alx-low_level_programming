e62bdfa60d7b6408be9fc22b609b5a352b2e7e50
edwinaikins <edwinaikins@gmail.com>
2022-03-22 00:40:30 +0000

    Half lies

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - prints
+ * @str: input
+ * Return: Nothing
+ */
+void puts2(char *str)
+{
+	int len = 0, i;
+
+	while (str[len] != '\0')
+		len++;
+	for (i = 0; i < len; i += 2)
+		_putchar(str[i]);
+	_putchar('\n')
+}
+
